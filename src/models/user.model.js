ed8bcf486dede6420bed4399353f9c04f8e8e735
airaawat <miraj.doshi@bosctechlabs.com>
2024-02-15 10:19:03 +0530

    new changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -47,6 +49,44 @@ const userSchema = new mongoose.Schema({
 {
     timestamps: true
 });
+
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+    
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+} );
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+};
+
+userSchema.methods.generateRefereshToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
